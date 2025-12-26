e4046b849852de7f6aac5c825c263faa8858a6e1
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-12-26 11:17:00 -0800

    ✨ feat(runner): add interactive test execution feature

--- runner.go
+++ runner.go
@@ -27,8 +27,7 @@ func main() {
 		runExecutableApp()
 		return
 	}
-	// todo
-	log.Println("Run Tests here ...")
+	runTests()
 }
 
 func getUserInput() (int, error) {
@@ -67,8 +66,9 @@ func runExecutableApp() {
 		os.Exit(0)
 	}
 
+	displayNewLine()
 	for i, v := range runnableFiles {
-		fmt.Printf("\n%d: '%s'\n", i+1, v)
+		fmt.Printf("%d: '%s'\n", i+1, v)
 	}
 	appNumberInput, err := getUserInput()
 	if err != nil {
@@ -79,7 +79,7 @@ func runExecutableApp() {
 	}
 	chosenFile := runnableFiles[appNumberInput-1]
 
-	fmt.Println("\n"+strings.Repeat("=", 18), "Go", strings.Repeat("=", 18))
+	fmt.Println("\n"+strings.Repeat("=", 18), "Go RUN", strings.Repeat("=", 18))
 	cmd := exec.Command("go", "run", chosenFile)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -87,5 +87,66 @@ func runExecutableApp() {
 		log.Printf("Program execution failed: %v", err)
 		os.Exit(1)
 	}
-	fmt.Println(strings.Repeat("=", 40))
+	fmt.Println(strings.Repeat("=", 44))
+}
+
+func runTests() {
+	var testPackages []string
+	packagesMap := make(map[string]bool)
+
+	if err := filepath.WalkDir("examples", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && strings.HasSuffix(d.Name(), "_test.go") {
+			dir := filepath.Dir(path)
+			pkgPath := filepath.ToSlash(dir)
+			if !strings.HasPrefix(pkgPath, "./") {
+				pkgPath = "./" + pkgPath
+			}
+
+			if !packagesMap[pkgPath] {
+				packagesMap[pkgPath] = true
+				testPackages = append(testPackages, pkgPath)
+			}
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("Error walking directory: %v", err)
+	}
+
+	testsSize := len(testPackages)
+	if testsSize == 0 {
+		fmt.Println("No '*_test.go' files found in 'examples' directory")
+		os.Exit(0)
+	}
+
+	displayNewLine()
+	for i, pkg := range testPackages {
+		fmt.Printf("%d: '%s'\n", i+1, pkg)
+	}
+
+	pkgNumber, err := getUserInput()
+	if err != nil {
+		log.Fatalf("Input error: %v", err)
+	}
+	if pkgNumber < 1 || pkgNumber > testsSize {
+		log.Fatalln("Input error: the number is out of the range")
+	}
+
+	chosenPackage := testPackages[pkgNumber-1]
+
+	fmt.Println("\n"+strings.Repeat("=", 18), "Go TEST", strings.Repeat("=", 18))
+	cmd := exec.Command("go", "test", "-v", chosenPackage)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Printf("Test execution failed: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println(strings.Repeat("=", 45))
+}
+
+func displayNewLine() {
+	fmt.Println()
 }
