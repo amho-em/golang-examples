cb2a9d754ad2905744307694b27d5408f8b32313
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-12-27 02:06:11 -0800

    ✨ feat(syntax): add values app1 demonstrating basic types and operations

---
+++ examples/syntax/values/app1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("String Concatenation:", "go"+"lang")
+
+	fmt.Println("1+1 =", 1+1)
+
+	fmt.Println("7.0/3 =", 7.0/3)
+	fmt.Println("7/3 =", 7/3)
+
+	fmt.Println("true && false =", true && false)
+	fmt.Println("true || false =", true || false)
+	fmt.Println("!true =", !true)
+}
