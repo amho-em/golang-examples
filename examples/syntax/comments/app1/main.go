29f3c79ec0dbb2c9ae72ab6a28343bcc2316d0e0
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-12-27 05:19:06 -0800

    ✨ feat(syntax): add comments example with various comment styles (#6)

---
+++ examples/syntax/comments/app1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// main displays information about me (documentation).
+func main() {
+	// Introduce myself (this is a single-line comment):
+	fmt.Println("I'm Amirhossein Emadi.")
+
+	fmt.Println("I'm 25 years old.") // Single-line comment ...
+
+	// fmt.Println("Ignore me!")
+
+	/*
+		This is a multi-line comment:
+		- line 1 ...
+		- line 2 ...
+		- line 3 ...
+	*/
+}
